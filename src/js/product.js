02ef85b0f012ecc58af879317c90e51d24364239
macsandoval1999 <msandoval2@byupathway.edu>
2026-01-12 21:31:56 -0700

    bug fix attempt 2

--- src/js/product.js
+++ src/js/product.js
@@ -6,7 +6,7 @@ const dataSource = new ProductData("tents");
 function addProductToCart(product) {
   // This function adds a product to the shopping cart stored in local storage. We pass in a product object to be added to the cart. If you look at the product JSONs, each product has an id, name, price, and other details, each one is an object. So when we add a product to the cart, we are adding the entire product object and we don't need to extract individual properties.
   // Get current cart from local storage or start with an empty array. The getLocalStorage function handles parsing JSON. It was already implemented in utils.mjs but wasn't being used here before. With it, we can create a list to store cart items.
-  let cartList = getLocalStorage("so-cart") || [];
+  const cartList = getLocalStorage("so-cart") || [];
   // Add the new product to the cart. The cartList array now holds all items in the cart, including the new product but hasn't been saved to local storage yet.
   cartList.push(product);
   // Now we save the updated cart back to local storage
